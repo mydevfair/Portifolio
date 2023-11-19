47bf0d448bb679d30c2df3a2ac7abd400364c1bf
cfairhurst5680@gmail.com <114191776+mydevfair@users.noreply.github.com>
2023-11-19 14:59:10 +0000

    Commit 19/11/23

--- src/Practical_13/Interpreter1.java
+++ src/Practical_13/Interpreter1.java
@@ -1,3 +1,10 @@
+/**
+ * File name: Interpreter1.java
+ * Author: Chris Fairhurst
+ * Date: November 19, 2023
+ * Description: This file contains the implementation of an interpreter
+ * for mathematical expressions, based on Java’s Stack.
+ */
 package Practical_13;
 
 import java.text.DecimalFormat;
@@ -5,19 +12,28 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Interpreter1 {
-    // Stack to store operands during the expression evaluation
+    /**
+     * Stack for storing operands and operators.
+     */
     private Stack<String> stack;
-    // Decimal format for formatting doubles in the stack
+
     private DecimalFormat decimalFormat;
 
-    // Constructor initializing the stack and decimal format
+    /**
+     * Constructor for Interpreter1.
+     */
     public Interpreter1() {
         this.stack = new Stack<>();
         this.decimalFormat = new DecimalFormat("#.##");
     }
 
+    /**
+     * Checks if the stack contains 3 operands and 1 operator.
+     * @param stack Stack to be checked.
+     * @return True if the stack contains 3 operands and 1 operator, false otherwise.
+     */
     public boolean checkStack(Stack<String> stack) {
-        if (this.stack.size() == 3 && !this.stack.contains("+, -, *, /")) {
+        if (this.stack.size() >= 4 && !this.stack.contains("+, -, *, /")) {
             System.out.println("Error: Input Error\n");
             return true;
         }
@@ -25,19 +41,20 @@ public class Interpreter1 {
         return false;
     }
 
-    //Method to process a postfix expression
+    /**
+     * Processes a postfix expression.
+     * @param postFixExpression Postfix expression to be processed.
+     */
     public void processPostFixExpression(String postFixExpression) {
-        // Separate tokens
+        // Tokenize input
         StringTokenizer tokenizer = new StringTokenizer(postFixExpression, " ");
         String token;
-
-        // Process input
+        // Process tokens
         while (tokenizer.hasMoreElements()) {
             token = tokenizer.nextToken();
 
-
+            // Check if token is numeric
             try {
-                // Check if token is numeric
                 Double tmpDouble = Double.parseDouble(token);
                 System.out.println("Rule 2: Operand (" + tmpDouble + ") := " + tmpDouble);
                 stack.push(String.valueOf(tmpDouble));
@@ -64,18 +81,24 @@ public class Interpreter1 {
                                 + formatResult(operand1) + ") Operand(" + formatResult(operand2) + ") Operator(" + token + ")");
                         System.out.println("Stack: " + stack);
                     } else {
-                        System.out.println("Error: Input Error\n");
+                        System.out.println("Error: Input Error1\n");
                         return;
                     }
                 } else {
-                    System.out.println("Error: Input Error\n");
+                    System.out.println("Error: Input Error2\n");
                     break;
                 }
             }
         }
     }
 
-    // Method to perform arithmetic operation based on the operator
+    /**
+     * Performs an operation on two operands.
+     * @param operand1 First operand.
+     * @param operand2 Second operand.
+     * @param operator Operator.
+     * @return Result of the operation.
+     */
     private Double performOperation(Double operand1, Double operand2, String operator) {
         switch (operator) {
             case "+":
@@ -94,15 +117,22 @@ public class Interpreter1 {
         }
     }
 
-    // Method to format a double using DecimalFormat
+    /**
+     * Formats a result to two decimal places.
+     * @param result Result to be formatted.
+     * @return Formatted result.
+     */
     private String formatResult(double result) {
         return decimalFormat.format(result);
     }
 
-    // Main method to test the interpreter
+    /**
+     * Main method for testing the Interpreter1.
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         Interpreter1 interpreter = new Interpreter1();
-        String postFixExpression = "4.3 25 - 1.7 2 * - 3.2 /";
+        String postFixExpression = "4.3 25 + 1.7 2 * - 3.2 /";
         interpreter.processPostFixExpression(postFixExpression);
     }
 }
